6071feacf7c3809899a8dddc03dcd32aaf43f5e9
Paul Czarkowski <username.taken@gmail.com>
2015-03-19 17:08:48 -0500

    storcli

--- sensu/plugins/check-raid.sh
+++ sensu/plugins/check-raid.sh
@@ -3,5 +3,9 @@
 if lspci | grep RAID | grep -i 3ware >> /dev/null; then
     sudo /etc/sensu/plugins/check_3ware_raid.py -b /usr/sbin/tw-cli
 elif lspci | grep RAID | grep -i "MegaRAID" >> /dev/null; then
-    sudo /etc/sensu/plugins/check_megaraid_sas.pl -b /usr/sbin/megacli
+    if [[ -e /etc/sensu/plugins/check_storcli.pl && -e /usr/sbin/storcli64 ]];then
+      sudo /etc/sensu/plugins/check_storcli.pl -b /usr/sbin/storcli64
+    else
+      sudo /etc/sensu/plugins/check_megaraid_sas.pl -b /usr/sbin/megacli
+    fi
 fi;
