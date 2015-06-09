97e39ee33cd4e4fbc61baefa30c3250feb8d5941
Josh Yotty <jyotty@bluebox.net>
2015-06-09 12:19:46 -0700

    better check for storcli presence

--- sensu/plugins/check-raid.sh
+++ sensu/plugins/check-raid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if lspci | grep RAID | grep -i 3ware >> /dev/null; then
+if lspci | grep RAID | grep -q -i 3ware; then
     sudo /etc/sensu/plugins/check_3ware_raid.py -b /usr/sbin/tw-cli
-elif lspci | grep RAID | grep -i "MegaRAID" >> /dev/null; then
-    if [[ -e /etc/sensu/plugins/check-storcli.pl && -e /usr/sbin/storcli64 ]];then
-      sudo /etc/sensu/plugins/check-storcli.pl -p /usr/sbin/storcli64
+elif lspci | grep RAID | grep -q -i MegaRAID; then
+    if type -p storcli64 storcli; then
+      sudo /etc/sensu/plugins/check-storcli.pl
     else
       sudo /etc/sensu/plugins/check_megaraid_sas.pl -b /usr/sbin/megacli -o 63
     fi
