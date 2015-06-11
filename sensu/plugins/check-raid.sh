7966dc7ba4092d84ec6d5a35267aea4870fc9216
Josh Yotty <jyotty@bluebox.net>
2015-06-11 11:33:36 -0700

    type returns nonzero if any argument is not present

--- sensu/plugins/check-raid.sh
+++ sensu/plugins/check-raid.sh
@@ -3,9 +3,9 @@
 if lspci | grep RAID | grep -q -i 3ware; then
     sudo /etc/sensu/plugins/check_3ware_raid.py -b /usr/sbin/tw-cli
 elif lspci | grep RAID | grep -q -i MegaRAID; then
-    if type -p storcli64 storcli; then
+    if type -p storcli64 >/dev/null || type -p storcli >/dev/null; then
       sudo /etc/sensu/plugins/check-storcli.pl
     else
       sudo /etc/sensu/plugins/check_megaraid_sas.pl -b /usr/sbin/megacli -o 63
     fi
-fi;
+fi
