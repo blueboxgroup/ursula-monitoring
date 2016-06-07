c77b1bf3be9e2eb4cd8001a9767d9a4f9908821f
Ulysses <ukanigel@bluebox.net>
2016-06-07 16:20:09 -0700

    Add criticality to mcelog client check script

--- sensu/plugins/check-mcelog.sh
+++ sensu/plugins/check-mcelog.sh
@@ -2,6 +2,14 @@
 # Queries the running mcelog daemon for accumulated errors.
 # by Ulysses Kanigel
 
+while getopts 'z:' OPT; do
+  case $OPT in
+    z)  CRITICALITY=$OPTARG;;
+  esac
+done
+
+CRITICALITY=${CRITICALITY:-critical}
+
 trap 'exit 1' ERR
 if mcelog --client | grep total | grep -v "0 total"; then
   echo "mcelog reports memory errors"
