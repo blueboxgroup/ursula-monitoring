c6d012a7ce3e92bd34bf299100321a4a717b594e
Xiaohua-Shen <xiaohua.shen@gmail.com>
2016-09-24 01:00:25 +0800

    Check ceph usage (#104)
    * check ceph usage by pool
    
    to support multi-end ceph
    
    * add specific pool option

--- sensu/plugins/check-ceph-usage.rb
+++ sensu/plugins/check-ceph-usage.rb
@@ -3,7 +3,7 @@
 # check-ceph-usage
 #
 # DESCRIPTION:
-#   Raise alert if ceph cluster %RAW used exceed threshold
+#   Raise alert if ceph pool %used exceed threshold
 #
 # OUTPUT:
 #   plain text, eixt code 0: OK, 1:Warning, 2:Critical, Others:Unknown
@@ -30,11 +30,17 @@
 #   Using -t (--threshold) option to determine alert level. 
 #   >= threshold, raise alert
 #
-# LICENSE:
-#   Copyright 2013 Brian Clark <brian.clark@cloudapt.com>
-#   Released under the same terms as Sensu (the MIT license); see LICENSE
-#   for details.
+#   Using -p (--pool) option to determine specific pools to check. 
+#   if not be provided, then all pools will be checked.
 #
+# Code adapted from Brian Clark's script in the Sensu Plugins community:
+# https://github.com/sensu-plugins/sensu-plugins-ceph/blob/master/bin/check-ceph.rb
+# with modification to support pool usage check.
+#
+# Released under the same terms as Sensu (the MIT license); see MITLICENSE
+# for details.
+#
+# Xiao Hua, Shen <shenxh@cn.ibm.com>
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
@@ -42,7 +48,7 @@ require 'timeout'
 require 'English'
 require 'json'
 
-class CheckCephClusterUsage < Sensu::Plugin::Check::CLI
+class CheckCephPoolUsage < Sensu::Plugin::Check::CLI
   option :keyring,
          description: 'Path to cephx authentication keyring file',
          short: '-k KEY',
@@ -86,6 +92,12 @@ class CheckCephClusterUsage < Sensu::Plugin::Check::CLI
          long: '--verbose',
          boolean: true,
          default: false
+  
+  option :pool,
+         description: 'only check specific pools',
+         short: '-p POOL',
+         long: '--pools POOL',
+         proc: proc {|a| a.split(',') }
 
   def run_cmd(cmd)
     pipe, status = nil
@@ -134,17 +146,22 @@ class CheckCephClusterUsage < Sensu::Plugin::Check::CLI
   def run
     result = run_cmd('ceph df --format=json')
     data = JSON.parse(result)
-    used_percentage = data['stats']['total_used_bytes'] * 100.0 / data['stats']['total_bytes']
-    
-    output = '%RAW Used: ' + used_percentage.to_s
-    output = output + '  ' + result if config[:verbose]
-    
-    if used_percentage >= config[:threshold]
-      warning output if config[:criticality] == 'warning'
-      critical output
-    end
-    
-    ok output
-	
+    crit_pool = []
+    output = ''
+
+    data['pools'].each do | pool |
+      next if config[:pool] && !config[:pool].include?(pool['name'])
+      usage = pool['stats']['bytes_used'] * 100.0 /(pool['stats']['bytes_used'] + pool['stats']['max_avail'])
+      crit_pool << "#{pool['name']} #{usage}%" if usage >= config[:threshold]
+    end unless result.to_s == ''
+
+    ok "All Pools usage under #{config[:threshold]}%" if crit_pool.empty?
+
+    output = crit_pool.join(', ')
+    output = output + "\n" + result if config[:verbose]
+
+    warning output if config[:criticality] == 'warning'
+    critical output
+
   end
 end
