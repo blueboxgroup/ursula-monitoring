596edd67786f5f925a574cd7269a105e1d9b0ac8
Xiaohua-Shen <xiaohua.shen@gmail.com>
2017-01-17 14:41:08 +0800

    commit
    1. no ifquery command in centos/rhel
    2. clean the output of check-cinder-service (RTC defect  294012)

--- sensu/plugins/check-ucarp-procs.sh
+++ sensu/plugins/check-ucarp-procs.sh
@@ -8,8 +8,21 @@ done
 
 CRITICALITY=${CRITICALITY:-critical}
 
-for IFACE in $(ifquery --list); do
-  for VIP in $(ifquery ${IFACE} | awk '/^ucarp-vip:/ {print $2}'); do
+if $(which ifquery >/dev/null 2>&1); then  # for ubuntu
+  for IFACE in $(ifquery --list); do
+    for VIP in $(ifquery ${IFACE} | awk '/^ucarp-vip:/ {print $2}'); do
+      if ! ps -ef | grep '/usr/sbin/ucarp' | grep ${VIP} >/dev/null; then
+        echo "no ucarp process is running for IP ${VIP}"
+        if [ "$CRITICALITY" == "warning" ]; then
+          exit 1
+        else
+          exit 2
+        fi
+      fi
+    done
+  done
+else # for centos/rhel
+  for VIP in $(awk '/^[^#]*VIP_ADDRESS/' /etc/ucarp/*.conf | sed 's/.*VIP_ADDRESS="\([^,]*\)"/\1/g'); do
     if ! ps -ef | grep '/usr/sbin/ucarp' | grep ${VIP} >/dev/null; then
       echo "no ucarp process is running for IP ${VIP}"
       if [ "$CRITICALITY" == "warning" ]; then
@@ -19,6 +32,6 @@ for IFACE in $(ifquery --list); do
       fi
     fi
   done
-done
+fi
 
 echo "All interfaces configured with uCARP have running process"
