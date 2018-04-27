7b9488802fc9ddbcc87c1d1c8a30df8ade8516ac
Andrew Cole <andrewcole@us.ibm.com>
2018-04-27 21:01:09 +0000

    removing mount checking in favor of check-fstab-mounts

--- sensu/plugins/check-fs-writable.sh
+++ sensu/plugins/check-fs-writable.sh
@@ -4,17 +4,12 @@
 mount_status=0
 
 for mount in $( findmnt -sno target | grep -v none ); do
-  if [[ ! -d $mount ]]; then
-    echo "FAIL - '${mount}' found in fstab, but not mounted!"
-    ((mount_status++))
-  else
-    file="${mount}/.tmp-writeable-check"
-    trap '[[ -f "${file}" ]] && rm "${file}"' TERM EXIT
+  file="${mount}/.tmp-writable-check"
+  trap '[[ -f "${file}" ]] && rm "${file}"' TERM EXIT
 
-    if ! touch "$file" 2>/dev/null; then
-      echo "FAIL - Could not create a file into '${mount}' !"
-      ((mount_status++))
-    fi
+  if ! touch "$file" 2>/dev/null; then
+    echo "FAIL - Could not create a file into '${mount}' !"
+    ((mount_status++))
   fi
 done
 
